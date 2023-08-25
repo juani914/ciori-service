defb33b6bd4a298523fdf920b1c131473196b6d9
juani914 <juanielizari@gmail.com>
2023-08-25 09:44:19 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburguer = document.querySelector(".hamburguer");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled',$(this).scrollTop()>50);
+});
